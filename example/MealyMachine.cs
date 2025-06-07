7028c9e1a6511449d2bc3cf4ac3cd79f1afe1374
Nilay Vishwakarma <nivishwa@microsof.com>
2025-06-07 12:31:29 +0530

    mealy and moore machine example

---
+++ example/MealyMachine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpFsm.Example
+{
+    public class MealyMachine
+    {
+        public enum MealyState { S0, S1 }
+        public class MealyContext
+        {
+            public string Input { get; set; }
+            public string Output { get; set; }
+        }
+
+        public MealyMachine()
+        {
+
+            var registry = new TransitionRegistry<MealyState, MealyContext>();
+            registry.RegisterSideEffect("OutputA", (ctx, _, _) => ctx.Output = "A");
+            registry.RegisterSideEffect("OutputB", (ctx, _, _) => ctx.Output = "B");
+
+            var builder = FiniteStateMachineBuilder<MealyState, MealyContext>.Create("Mealy")
+                .WithInitialState(MealyState.S0)
+                .WithRegistry(registry)
+                .AddTransition(MealyState.S0, MealyState.S1)
+                    .When(ctx => ctx.Input == "x")
+                    .WithSideEffect("OutputA")
+                    .Done()
+                .AddTransition(MealyState.S1, MealyState.S0)
+                    .When(ctx => ctx.Input == "y")
+                    .WithSideEffect("OutputB")
+                    .Done();
+            var fsm = new FiniteStateMachine<MealyState, MealyContext>(builder.Build());
+        }
+    }
+}
---
+++ example/MooreMachine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpFsm.Example
+{
+    public class MooreMachine
+    {
+        public enum MooreState { S0, S1 }
+        public class MooreContext
+        {
+            public string Output { get; set; }
+        }
+
+        public MooreMachine()
+        {
+            var registry = new TransitionRegistry<MooreState, MooreContext>();
+            // No side effects on transitions; output is determined by state
+
+            var builder = FiniteStateMachineBuilder<MooreState, MooreContext>.Create("Moore")
+                .WithInitialState(MooreState.S0)
+                .WithRegistry(registry)
+                .AddTransition(MooreState.S0, MooreState.S1)
+                    .WithSideEffect((MooreContext ctx, MooreState oldState, MooreState newState) =>
+                    {
+                        switch (newState)
+                        {
+                            case MooreState.S0: ctx.Output = "A"; break;
+                            case MooreState.S1: ctx.Output = "B"; break;
+                        }
+                    })
+                    .Done()
+                .AddTransition(MooreState.S1, MooreState.S0)
+                    .Done();
+
+            var fsm = new FiniteStateMachine<MooreState, MooreContext>(builder.Build());
+        }
+    }
+}
