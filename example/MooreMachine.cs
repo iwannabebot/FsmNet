428dda27e13fc588dcd0133adee1a8c8e2303b81
Nilay Vishwakarma <30928429+iwannabebot@users.noreply.github.com>
2025-06-08 15:57:21 +0530

    Update MooreMachine.cs

--- example/MooreMachine.cs
+++ example/MooreMachine.cs
@@ -17,22 +17,23 @@ namespace SharpFsm.Example
         public MooreMachine()
         {
             var registry = new TransitionRegistry<MooreState, MooreContext>();
-            // No side effects on transitions; output is determined by state
+            registry.RegisterSideEffect("OutputSideEffect", (MooreContext ctx, MooreState oldState, MooreState newState) => {
+                switch (newState) 
+                {
+                    case MooreState.S0: ctx.Output = "A"; break;
+                    case MooreState.S1: ctx.Output = "B"; break;
+                    default: ctx.Output = "C"; break;
+                }
+            });
 
             var builder = FiniteStateMachineBuilder<MooreState, MooreContext>.Create("Moore")
                 .WithInitialState(MooreState.S0)
                 .WithRegistry(registry)
                 .AddTransition(MooreState.S0, MooreState.S1)
-                    .WithSideEffect((MooreContext ctx, MooreState oldState, MooreState newState) =>
-                    {
-                        switch (newState)
-                        {
-                            case MooreState.S0: ctx.Output = "A"; break;
-                            case MooreState.S1: ctx.Output = "B"; break;
-                        }
-                    })
+                    .WithSideEffect("OutputSideEffect")
                     .Done()
                 .AddTransition(MooreState.S1, MooreState.S0)
+                    .WithSideEffect("OutputSideEffect")
                     .Done();
 
             var fsm = new FiniteStateMachine<MooreState, MooreContext>(builder.Build());
